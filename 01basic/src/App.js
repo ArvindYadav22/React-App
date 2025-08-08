5eff5155b94cb2915edb675165b490c428db5246
ArvindYadav22 <155368175+ArvindYadav22@users.noreply.github.com>
2025-08-08 20:24:43 +0530

    commit

---
+++ 01basic/src/App.js
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <h2>Welcome to react world</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+function App() {
+
+
+  return (
+   <div>
+    <h1> Hello world</h1>
+    <h2>WELCOME TO THE WORLD OF VITE REACT</h2>
+   </div>
+        
+  )
+}
+
+export default App
