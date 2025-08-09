9f8886bf6f697d84d11d02c3151cd8d30503c28a
ArvindYadav22 <155368175+ArvindYadav22@users.noreply.github.com>
2025-08-10 01:30:59 +0530

    commit

---
+++ 02hook/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+
+  const addvalue = () => {
+    setCount(count + 1)
+  }
+  const removevalue = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Hello Guys</h1>
+        <h2>counter value {count}</h2>
+        <button onClick={addvalue}>add value {count}</button>
+        <br />
+        <button onClick={removevalue}>remove value {count}</button>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
